5fc6e6b5b96c3d097579e3ad3be3afdd43335ba5
SiteHUB-NG <infra-team@sitehub.agency>
2026-03-04 15:09:21 +0000

    Update cron.sh
    Signed-off-by: SiteHUB-NG <infra-team@sitehub.agency>

--- cron.sh
+++ cron.sh
@@ -13,7 +13,11 @@
 # === Build: Jan 2026 === # === Updated: Feb 2026 == # === Version#: 1.2.5 === #
 # ====== One-Click ====== #
 # ==== Cron logic ====
-install_dep "cron" "command -v crontab" "crond" "$pkg_mgr" true
+case "$id" in
+  debian) install_dep "cron" "command -v crontab" "cron" "$pkg_mgr" true                 ;;
+  rhel|centos|fedora) install_dep "cronie" "command -v crontab" "cronie" "$pkg_mgr" true ;;
+   *) printf '%s\n' "Unknown OS: $id"                                                    ;;
+esac
 trap 'cron_menu' SIGUSR1
 draw_table_row() {
   local index="$1"
